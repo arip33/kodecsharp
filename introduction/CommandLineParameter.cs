624f4115d7509b290f55db564af956eac6c23d89
wsaryada <wsaryada@yahoo.com>
2012-01-17 13:35:43 +0800

    Initial project solution commit.

---
+++ introduction/CommandLineParameter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kodecsharp.Example.Intro
+{
+    class CommandLineParameter
+    {
+        /// <summary>
+        /// Reading command line parameter.
+        /// </summary>
+        /// <param name="args">command line parameteres</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Argument size: " + args.Length);
+
+            //
+            // Print out the entire command line parameters passed 
+            // into this program.
+            //
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Argument[" + i + "] : " + args[i]);
+            }
+        }
+    }
+}
---
+++ introduction/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿namespace Kodecsharp.Example.Intro
+{
+    /// <summary>
+    /// HelloWorld in C#
+    /// </summary>
+    class HelloWorld
+    {
+        public static void Main()
+        {
+            //
+            // Say hello to the world!
+            //
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
