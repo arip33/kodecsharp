c74e7e655ba5c6a067769227d20ea992e8ecde3e
wsaryada <wsaryada@yahoo.com>
2012-01-27 10:43:12 +0800

    Update examples.

---
+++ introduction/AutoImplementedProperty.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Kodecsharp.Example.Intro
+{
+    class AutoImplementedProperty
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            UserAccount user = new UserAccount(1);
+            user.Username = "admin";
+            user.FirstName = "System";
+            user.LastName = "Administrator";
+
+            Console.WriteLine(user);
+        }
+    }
+
+    class UserAccount
+    {
+        //
+        // Auto-implemented properties
+        //
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Creates a new UserAccount with the specified Id.
+        /// </summary>
+        /// <param name="id">a UserAccount Id</param>
+        public UserAccount(long id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// A string representation of this object.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("Id: {0}; Username: {1}; " + 
+                "FirstName: {2}; LastName: {3}", 
+                Id, Username, FirstName, LastName);
+        }
+    }
+}
---
+++ introduction/PropertyDemo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Kodecsharp.Example.Intro
+{
+    class PropertyDemo
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            User user = new User();
+            user.Id = 1;
+            user.Username = "admin";
+            user.FirstName = "System";
+            user.LastName = "Administrator";
+
+            System.Console.WriteLine("User: " + user.Id + "; " +
+                user.Username + "; " + user.FirstName + "; " +
+                user.LastName);
+        }
+    }
+
+    class User
+    {
+        private long id;
+        private string username;
+        private string firstName;
+        private string lastName;
+
+        public User()
+        {
+        }
+
+        /// <summary>
+        /// User ID Property
+        /// </summary>
+        public long Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        /// <summary>
+        /// Username Property
+        /// </summary>
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        /// <summary>
+        /// Firstname Property
+        /// </summary>
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        /// <summary>
+        /// Lastname Property
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }        
+    }
+}
