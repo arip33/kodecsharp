c7e4d2a1f20b7c2bf3776f0f809a301cb928fd85
wsaryada <wsaryada@yahoo.com>
2012-02-03 20:07:42 +0800

    Update code example

---
+++ introduction/ForeachDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kodecsharp.Example.Intro
+{
+    class ForeachDemo
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Argument size: " + args.Length);
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine("Argument: " + arg);
+            }
+        }
+    }
+}
