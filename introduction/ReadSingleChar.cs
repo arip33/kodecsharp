e8cfc5364f215308e4110058c321ed23d1f4b7f1
I Wayan Saryada <wsaryada@yahoo.com>
2012-06-14 19:54:51 +0800

    update examples

---
+++ introduction/ReadSingleChar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Kodecsharp.Example.Intro
+{
+    class ReadSingleChar
+    {
+        static void Main()
+        {
+            Console.WriteLine("Menus");
+            Console.WriteLine("==================");
+            Console.WriteLine("1. Friend Chicken");
+            Console.WriteLine("2. Burger");
+            Console.WriteLine("3. Sandwich");
+            Console.WriteLine("4. Hot Dog");
+            Console.WriteLine("==================");
+            Console.Write("Enter your selection: ");
+
+            int readValue = Console.Read();
+            char character = (char) readValue;
+
+            //
+            // Check if user enter value 1 to 4. Other input will execute the
+            // else block.
+            //
+            if (readValue >= 49 && readValue <= 52)
+            {
+                Console.WriteLine("You've choosen menu number: {0}", character);
+                Console.WriteLine("Read value                : {0}", readValue);
+                Console.WriteLine("Character value           : {0}", character);
+            }
+            else
+            {
+                Console.WriteLine("We don't have anything for you");
+                Console.WriteLine("You've entered: {0}, {1}", readValue, character);
+            }
+
+        }
+    }
+}
---
+++ introduction/ReadSingleKey.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kodecsharp.Example.Intro
+{
+    class ReadSingleKey
+    {
+        static void Main()
+        {            
+            ConsoleKeyInfo info;
+
+            //
+            // Read user input until user presses 'N'
+            //
+            do
+            {
+                Console.Write("Do you want to continue the process? (Y/N): ");
+                info = Console.ReadKey();
+                if (info.Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Continue...");
+                }
+            } while (info.Key != ConsoleKey.N);
+            Console.WriteLine("");
+            Console.WriteLine("Exit...");
+        }
+    }
+}
